36809be68f4ea7671840b150edf262076c421e8d
YunFeng6666 <120697095+YunFeng6666@users.noreply.github.com>
2024-01-27 22:09:07 +0900

    Add files via upload

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6789
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6789 \
     libgptutils \
     libz \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_yunluo-user
-add_lunch_combo omni_yunluo-userdebug
-add_lunch_combo omni_yunluo-eng
+#add_lunch_combo omni_yunluo-user
+#add_lunch_combo omni_yunluo-userdebug
+#add_lunch_combo omni_yunluo-eng
